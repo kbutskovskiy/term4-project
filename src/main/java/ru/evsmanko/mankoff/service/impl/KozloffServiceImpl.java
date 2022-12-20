086c38aa85bf37994d779dc77d9972d21c526ded
ssk11 <ssk29@yandex.ru>
2022-12-20 15:49:54 +0300

    add package impl

--- src/main/java/ru/evsmanko/mankoff/service/impl/KozloffServiceImpl.java
+++ src/main/java/ru/evsmanko/mankoff/service/impl/KozloffServiceImpl.java
@@ -1,8 +1,9 @@
-package ru.evsmanko.mankoff.service;
+package ru.evsmanko.mankoff.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.evsmanko.mankoff.service.KozloffService;
 
 @Slf4j
 @Service
