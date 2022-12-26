6591d3d49bd590fd83fdb7632ab9497601543417
Kirill Buckovsky <kbutskovskiy@mail.ru>
2022-12-26 22:29:47 +0300

    Add configs

--- src/main/java/ru/evsmanko/mankoff/service/impl/BuckovskyServiceImpl.java
+++ src/main/java/ru/evsmanko/mankoff/service/impl/BuckovskyServiceImpl.java
@@ -1,12 +1,15 @@
 package ru.evsmanko.mankoff.service.impl;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.evsmanko.mankoff.repository.UserRepository;
 import ru.evsmanko.mankoff.service.BuckovskyService;
 import ru.evsmanko.mankoff.repository.CreditRepository;
 import ru.evsmanko.mankoff.entity.Credit;
 import ru.evsmanko.mankoff.entity.User;
+import java.lang.String;
 
 import java.util.List;
 
@@ -16,6 +19,14 @@ import java.util.List;
 public class BuckovskyServiceImpl implements BuckovskyService{
     private final CreditRepository creditRepository;
     private final UserRepository userRepository;
+    @Value("${app.currency}")
+    private String currency;
+    @Value("${app.USD}")
+    private double coefficientUSD;
+    @Value("${app.RUB}")
+    private double coefficientRUB;
+    @Value("${app.EUR}")
+    private double coefficientEUR;
     public String creditSum(long id){
         User user = userRepository.getUserById(id);
         if (user == null){
@@ -25,6 +36,11 @@ public class BuckovskyServiceImpl implements BuckovskyService{
         List<Credit> creditList = creditRepository.findAllByUserId(id);
         for (Credit credit : creditList) {
             sumCredit += credit.getAmount();
+        }
+        switch (currency) {
+            case ("RUB") -> sumCredit *= coefficientRUB;
+            case ("EUR") -> sumCredit *= coefficientEUR;
+            case ("USD") -> sumCredit *= coefficientUSD;
         }
         return String.valueOf(sumCredit);
     }
