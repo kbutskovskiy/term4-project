db50faeb4453fbcc99c177efb3af8aa15c9b91eb
lantum1 <denisustinov2003@mail.ru>
2022-12-27 11:39:36 +0300

    Initial Commit of HW 8

--- src/main/java/ru/evsmanko/mankoff/controller/UstinoffController.java
+++ src/main/java/ru/evsmanko/mankoff/controller/UstinoffController.java
@@ -2,10 +2,7 @@ package ru.evsmanko.mankoff.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.evsmanko.mankoff.service.UstinoffService;
 
 @Slf4j
@@ -14,11 +11,13 @@ import ru.evsmanko.mankoff.service.UstinoffService;
 @RequestMapping("ustinoff-service")
 public class UstinoffController {
     private final UstinoffService ustinoffService;
-
     @GetMapping("/userInfoToJson/{id}")
-    public void userInfoToJson(@PathVariable int id) {
+    public String userInfoToJson(@PathVariable int id) {
         log.info("START endpoint usinoffService/exportToJson, id:{}", id);
         String userInfo = ustinoffService.exportToJson(id);
         log.info("END endpoint usinoffService/exportToJson, user info:\n{}",userInfo);
+        return userInfo;
     }
+
+
 }
