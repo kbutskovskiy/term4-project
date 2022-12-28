c57e824279fdb00ba2eeda876ef65c5b44252b1b
lantum1 <denisustinov2003@mail.ru>
2022-12-28 19:30:02 +0300

    HW 9 Ustinov

--- src/main/java/ru/evsmanko/mankoff/service/impl/UstinoffServiceImpl.java
+++ src/main/java/ru/evsmanko/mankoff/service/impl/UstinoffServiceImpl.java
@@ -2,6 +2,7 @@ package ru.evsmanko.mankoff.service.impl;
 import com.google.gson.GsonBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 import ru.evsmanko.mankoff.entity.User;
 import ru.evsmanko.mankoff.repository.UserRepository;
 import ru.evsmanko.mankoff.service.UstinoffService;
@@ -13,13 +14,15 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class UstinoffServiceImpl implements UstinoffService {
     private final UserRepository userRepository;
+    @Value("${app.jsonName}")
+    private String jsonName;
     @Override
     public String exportToJson(long id) {
         User user = userRepository.getUserById(id);
         FileWriter writer = null;
         String json = null;
         try {
-            writer = new FileWriter("jsons/user"+id+".json");
+            writer = new FileWriter("jsons/"+jsonName+".json");
             json = new GsonBuilder().setPrettyPrinting().create().toJson(user);
             writer.write(json);
             writer.close();
