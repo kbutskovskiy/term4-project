80ff6488387f3ed69cc97bb426b9a3209aa75ff3
Kirill Buckovsky <kbutskovskiy@mail.ru>
2023-02-06 21:39:13 +0300

    Deleted coments in my controller

--- src/main/java/ru/evsmanko/mankoff/configuration/BuckovskySpringMVCDispatcherServletInitializer.java
+++ src/main/java/ru/evsmanko/mankoff/configuration/BuckovskySpringMVCDispatcherServletInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class BuckovskySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class[0];
     }
 
     @Override
