ba1839775d9f0d7b7e2b4b1026929e29851b3094
Mikhaylov Mikhail <withfuckinglove@mail.ru>
2023-02-10 11:47:07 +0300

    resolve conflict dz10

--- src/main/java/ru/evsmanko/mankoff/configuration/SpringMVCConfiguration.java
+++ src/main/java/ru/evsmanko/mankoff/configuration/SpringMVCConfiguration.java
@@ -26,7 +26,7 @@ public class SpringMVCConfiguration implements WebMvcConfigurer {
     public SpringResourceTemplateResolver templateResolver(){
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/WEB-INF.views/");
+        templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
         return  templateResolver;
     }
