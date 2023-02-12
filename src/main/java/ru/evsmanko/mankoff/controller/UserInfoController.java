5610e96140cae0656078690d01697ea7edcf0dca
Kirill Buckovsky <kbutskovskiy@mail.ru>
2023-02-12 16:51:49 +0300

    Done task 11

--- src/main/java/ru/evsmanko/mankoff/controller/UserInfoController.java
+++ src/main/java/ru/evsmanko/mankoff/controller/UserInfoController.java
@@ -1,17 +1,22 @@
-
 package ru.evsmanko.mankoff.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 
 @Slf4j
 @Controller
 public class UserInfoController {
     @GetMapping("/user")
-    public String showUserInfo() {
+    public String showUserInfo(Model model) {
+        model.addAttribute("nameAndSurname", "Кирилл Буцковский");
+        model.addAttribute("cardBalance", "1240");
+        model.addAttribute("depositBalance", "56300");
+        model.addAttribute("creditSum", "5600");
+        model.addAttribute("phoneBalance", "2");
         return "UserInfo";
     }
 }
