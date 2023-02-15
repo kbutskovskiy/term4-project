9e5b9ca087991f95f293019c33434246529056d1
lantum <denisustinov2003@mail.ru>
2023-02-15 13:51:17 +0300

    dz11 Ustinov In Com

--- src/main/java/ru/evsmanko/mankoff/controller/ContactsController.java
+++ src/main/java/ru/evsmanko/mankoff/controller/ContactsController.java
@@ -2,14 +2,18 @@ package ru.evsmanko.mankoff.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class ContactsController {
-
     @GetMapping("/contacts")
-    public String getContacts() {
+    public String getContacts(Model model) {
+        model.addAttribute("fullAddress","Russia, Moscow, Andropov avenue, 18, building 1")
+                .addAttribute("telephonePrivate1","8 800 250-0-520")
+                .addAttribute("telephonePrivate2", " +7 495 777-000-1")
+                .addAttribute("telephoneBusiness1", "8 800 250-0-199");
         return "contacts";
     }
 }
