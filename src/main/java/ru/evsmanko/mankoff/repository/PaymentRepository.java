a8aeaafc80f94db8e780a9f7c6926546a1d01056
Kirill Buckovsky <kbutskovskiy@mail.ru>
2023-02-17 09:21:32 +0300

    Dz13 started

--- src/main/java/ru/evsmanko/mankoff/repository/PaymentRepository.java
+++ src/main/java/ru/evsmanko/mankoff/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package ru.evsmanko.mankoff.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import ru.evsmanko.mankoff.entity.Payment;
 
@@ -8,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public interface PaymentRepository extends CrudRepository<Payment, Long> {
+public interface PaymentRepository {
     ArrayList<Payment> findAll();
 
     List<Payment> getPaymentsByUserId(long userId);
---
+++ src/main/java/ru/evsmanko/mankoff/repository/impl/PaymentRepositoryImpl.java
@@ -0,0 +1,51 @@
+package ru.evsmanko.mankoff.repository.impl;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.evsmanko.mankoff.entity.Payment;
+import ru.evsmanko.mankoff.repository.PaymentRepository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class PaymentRepositoryImpl implements PaymentRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    @Override
+    public ArrayList<Payment> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Payment> getPaymentsByUserId(long userId) {
+        return jdbcTemplate.query(
+                "select id, userId, mccCode, amount, timeStamp from Payment where userId=?",
+                this::mapRowToPayment, userId);
+    }
+
+    @Override
+    public Payment save(Payment payment) {
+        jdbcTemplate.update(
+                "insert into Payment (id, userId, mccCode, amount, timeStamp) values(?, ?, ?, ?, ?)",
+                payment.getId(),
+                payment.getUserId(),
+                payment.getMccCode(),
+                payment.getAmount(),
+                payment.getTimeStamp());
+        return payment;
+    }
+
+    private Payment mapRowToPayment(ResultSet row, int rowNum)
+        throws SQLException{
+        return new Payment(row.getLong("id"), row.getLong("userId"), row.getInt("mccCode"),
+                row.getDouble("amount"), row.getDouble("timeStamp"));
+    }
+}
