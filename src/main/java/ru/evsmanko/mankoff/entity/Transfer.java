377a39e3ef72ab8ce7a72e371b1f3bb6a272d906
lantum <denisustinov2003@mail.ru>
2023-02-23 01:12:03 +0300

    dz13 Ustinov In Com

--- src/main/java/ru/evsmanko/mankoff/entity/Transfer.java
+++ src/main/java/ru/evsmanko/mankoff/entity/Transfer.java
@@ -6,13 +6,20 @@ import lombok.*;
 @Setter
 @Getter
 @Entity
-@Table(name = "tranfers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TRANSFER")
 public class Transfer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long transferId;
+
+    @Column(name="ID")
+    private long id;
+    @Column(name="SENDER_ID")
     private long senderId;
+    @Column(name="RECIEVER_ID")
     private long recieverId;
+    @Column(name="AMOUNT")
     private double amount;
+    @Column(name="TIME_STAMP")
     private double timeStamp=1;
 }
